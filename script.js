66a3379b6c2b666fb4d152b51342b0e41742cb3c
subashpers <subashmoviestime@gmail.com>
2026-02-02 09:08:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
 const result = document.getElementById("result");
+
+let scale = 1;
+
+noBtn.addEventListener("click", () => {
+  scale -= 0.15;
+  noBtn.style.transform = `scale(${scale})`;
+
+  if (scale <= 0.3) {
+    noBtn.style.display = "none";
+  }
+});
 
 yesBtn.addEventListener("click", () => {
   result.innerHTML = "Yay! 💕 I can’t wait to spend Valentine’s Day with you 💖";
   yesBtn.style.display = "none";
+  noBtn.style.display = "none";
 });
